385fd8f128482c501c4fabfa93ec31aa523df951
victor cordeiro <victor.cdp97@gmail.com>
2021-05-15 12:35:10 -0300

    did auth homework

---
+++ Rails/Rails Auth/Rails Auth Homework/auth_homework/app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord
+    validates :username, :session_token, presence: true
+    validates :username, uniqueness: true
+    validates :password_digest, presence: { message: "password can't be blank"}
+    validates :password, length: {minimum: 6, allow_nil: true}
+
+    before_validation :ensure_session_token
+
+    attr_reader :password
+
+    def self.find_by_credentials(username, password)
+        User.find_by(username: username, password: password)
+    end
+
+    def password=(newpassword)
+        @password = newpassword
+        self.password_digest = BCrypt::Password.create(@password)
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+        self.session_token = self.class.generate_session_token
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= self.class.generate_session_token
+    end
+end

40df8cdcabd04da8257d75edaf733fc9aa96549f
victor cordeiro <victor.cdp97@gmail.com>
2021-05-17 09:57:04 -0300

    did login and logout for users, done with phase 4

--- Rails/Rails Auth/99 Cats 2/cats/app/controllers/sessions_controller.rb
+++ Rails/Rails Auth/99 Cats 2/cats/app/controllers/sessions_controller.rb
@@ -1,17 +1,23 @@
 class SessionsController < ApplicationController
+    before_action :require_no_user!, only: [:new, :create]
+
     def new
         render :new
     end
     
     def create
         user = User.find_by_credentials(params[:user][:user_name], params[:user][:password])
-        token = user.reset_session_token!
-        session[:session_token] = token
+        login_user!(user)
         redirect_to cats_url
     end
 
     def destroy
-
+        user = current_user
+        if user
+            user.reset_session_token!
+            session[:session_token] = nil
+        end
+        redirect_to cats_url
     end
 
 end
--- Rails/Rails Auth/99 Cats 2/cats/app/controllers/users_controller.rb
+++ Rails/Rails Auth/99 Cats 2/cats/app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :require_no_user!, only: [:new, :create]
+
     def new
         @user = User.new
         render :new
@@ -8,6 +10,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
+            login_user!(@user)
             redirect_to cats_url
         else
             render :new

1e542583dc1d57de862f6016ea2cc52650377098
victor cordeiro <victor.cdp97@gmail.com>
2021-05-22 09:53:20 -0300

    did better lyrics, done with phase 3

--- Rails/Rails Auth 2/Music App/music_app/app/helpers/application_helper.rb
+++ Rails/Rails Auth 2/Music App/music_app/app/helpers/application_helper.rb
@@ -4,4 +4,20 @@ module ApplicationHelper
         "<input type='hidden' name='authenticity_token'
         value=#{form_authenticity_token}>".html_safe
     end
+
+    def ugly_lyrics(lyrics)
+        pretty_lyrics = ""
+
+        lyrics.each_line do |line|
+            if line.match(/^[\s]*$\n/)
+                pretty_lyrics << line
+            else
+                pretty_lyrics << ("&#9835; " + line)
+            end
+        end
+
+        "<pre>#{pretty_lyrics}
+        </pre>".html_safe
+    end
+    
 end
